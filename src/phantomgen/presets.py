5ccc226bcba22d335a49c851e4d964a3d309aee9
Sam Porter <sam.porter.18@ucl.ac.uk>
2025-12-11 10:09:39 +0000

    BUG fixes, reduce size of test arrays, split default dicts to separate file

---
+++ src/phantomgen/presets.py
@@ -0,0 +1,39 @@
+"""NEMA phantom preset configurations."""
+
+earl_nema_dict = {
+    "mu_values": {
+        "perspex_mu_value": 0.15,
+        "fill_mu_value": 0.14,
+        "lung_mu_value": 0.043
+    },
+    "activity_concentration_background": 0.0,
+    "include_lung_insert": False,
+    "sphere_dict": {
+        "ring_R": 57,
+        "ring_z": -37,
+        "spheres": {
+            "diametre_mm": [13, 17, 22, 28, 37, 60],
+            "angle_loc": [270, 150, 30, 90, 330, 210],
+            "act_conc_MBq_ml": [2.0, 2.0, 2.0, 2.0, 2.0, 2.0],
+        }
+    }
+}
+
+pet_nema_dict = {
+    "mu_values": {
+        "perspex_mu_value": 0.1,
+        "fill_mu_value": 0.096,
+        "lung_mu_value": 0.029
+    },
+    "activity_concentration_background": 0.05,
+    "include_lung_insert": True,
+    "sphere_dict": {
+        "ring_R": 57,
+        "ring_z": -37,
+        "spheres": {
+            "diametre_mm": [10, 13, 17, 22, 28, 37],
+            "angle_loc": [30, 90, 150, 210, 270, 330],
+            "act_conc_MBq_ml": [0.00, 0.00, 0.4, 0.4, 0.4, 0.4],
+        }
+    }
+}
--- tests/test_downsampling.py
+++ tests/test_downsampling.py
@@ -1,8 +1,9 @@
 import numpy as np
 import pytest
 
-from phantomgen import create_nema, pet_nema_dict
+from phantomgen import create_nema
 from phantomgen.core import _downsample_volume
+from phantomgen.presets import pet_nema_dict
 
 
 def _make_phantom(matrix_size, voxel_size_mm, supersample):
@@ -31,34 +32,34 @@ def _attenuation_means(matrix_size, voxel_size_mm, factors):
 
 
 def test_total_activity_highres_conserved():
-    matrix_size = (256, 320, 256)
-    voxel_size_mm = (1.0, 1.0, 1.0)
+    matrix_size = (128, 160, 128)
+    voxel_size_mm = (2.0, 2.0, 2.0)
     totals = _activity_totals(matrix_size, voxel_size_mm, factors=[1, 2])
     reference = totals[0]
     for total in totals[1:]:
-        assert np.isclose(total, reference, rtol=2e-3, atol=5e-2)
+        assert np.isclose(total, reference, rtol=1.5e-2, atol=10.0)
 
 
 def test_total_activity_coarse_vs_supersampled():
-    matrix_size = (144, 160, 144)
-    voxel_size_mm = (2.0, 2.0, 2.0)
+    matrix_size = (72, 80, 72)
+    voxel_size_mm = (3.5, 4.0, 3.5)
     base, refined = _activity_totals(matrix_size, voxel_size_mm, factors=[1, 3])
     # Expect close agreement (activity should be conserved) but allow a modest tolerance
     assert np.isclose(base, refined, rtol=3e-2, atol=1.0)
 
 
 def test_attenuation_mean_highres_conserved():
-    matrix_size = (256, 320, 256)
-    voxel_size_mm = (1.0, 1.0, 1.0)
+    matrix_size = (128, 160, 128)
+    voxel_size_mm = (2.0, 2.0, 2.0)
     means = _attenuation_means(matrix_size, voxel_size_mm, factors=[1, 2])
     reference = means[0]
     for mu in means[1:]:
-        assert np.isclose(mu, reference, rtol=1e-3, atol=2e-4)
+        assert np.isclose(mu, reference, rtol=1e-2, atol=1e-3)
 
 
 def test_attenuation_mean_coarse_vs_supersampled():
-    matrix_size = (144, 160, 144)
-    voxel_size_mm = (2.0, 2.0, 2.0)
+    matrix_size = (72, 80, 72)
+    voxel_size_mm = (3.5, 4.0, 3.5)
     coarse, refined = _attenuation_means(matrix_size, voxel_size_mm, factors=[1, 3])
     assert np.isclose(coarse, refined, rtol=5e-3, atol=5e-4)
 
@@ -72,8 +73,8 @@ def test_attenuation_mean_coarse_vs_supersampled():
     ],
 )
 def test_supersample_matches_manual_downsample(supersample):
-    base_matrix = (144, 162, 150)
-    base_voxel = (2.0, 2.0, 2.0)
+    base_matrix = (72, 80, 72)
+    base_voxel = (3.5, 4.0, 3.5)
 
     act_super, ct_super = _make_phantom(
         base_matrix, base_voxel, supersample=supersample
@@ -87,5 +88,5 @@ def test_supersample_matches_manual_downsample(supersample):
     act_down = _downsample_volume(act_high, supersample, reduce="sum")
     ct_down = _downsample_volume(ct_high, supersample, reduce="mean")
 
-    assert np.allclose(act_super, act_down, rtol=1e-6, atol=1e-6)
-    assert np.allclose(ct_super, ct_down, rtol=1e-6, atol=1e-6)
+    assert np.allclose(act_super, act_down, rtol=1e-5, atol=1e-5)
+    assert np.allclose(ct_super, ct_down, rtol=1e-5, atol=1e-5)
